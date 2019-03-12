747b85504a3dd3089b13f3cc1fe3f3fce0613b70
Sukru Alatas <alatas@gmail.com>
2019-03-12 21:15:48 +0300

    c2

--- UnitTestExtensions/Class1.cs
+++ UnitTestExtensions/Class1.cs
@@ -1,36 +1,43 @@
 ﻿using Microsoft.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
 using System;
+using System.Linq;
+using System.Threading;
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 public class KernelTraceSessionAttribute : TraceSessionAttribute
 {
-    public KernelTraceSessionAttribute(KernelTraceEventParser.Keywords KernelSession = KernelTraceEventParser.Keywords.None,
+    public KernelTraceSessionAttribute(KernelTraceEventParser.Keywords KernelSessionKeywords = KernelTraceEventParser.Keywords.None,
         string[] acceptedEventProviderNames = null,
         string[] rejectedEventProviderNames = null,
         string[] acceptedEventNames = null,
         string[] rejectedEventNames = null,
         string[] inclusiveProcessNames = null,
         int[] includedEventIds = null,
-        int[] excludedEventIds = null) : base("kernel", KernelTraceEventParser.KernelSessionName,
-        TraceEventLevel.Verbose,
-        acceptedEventProviderNames,
-        rejectedEventProviderNames,
-        acceptedEventNames,
-        rejectedEventNames,
-        inclusiveProcessNames,
-        includedEventIds,
-        excludedEventIds)
+        int[] excludedEventIds = null) : base("kernel",
+            typeof(KernelTraceEventParser),
+            KernelTraceEventParser.KernelSessionName,
+            TraceEventLevel.Verbose,
+            acceptedEventProviderNames,
+            rejectedEventProviderNames,
+            acceptedEventNames,
+            rejectedEventNames,
+            inclusiveProcessNames,
+            includedEventIds,
+            excludedEventIds)
     {
-
+        this.KernelSessionKeywords = KernelSessionKeywords;
     }
 
+    public readonly KernelTraceEventParser.Keywords KernelSessionKeywords;
 }
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 public class TraceSessionAttribute : Attribute
 {
     public TraceSessionAttribute(string providerName,
+        Type eventParserType,
         string sessionName = null,
         TraceEventLevel providerLevel = TraceEventLevel.Verbose,
         string[] acceptedEventProviderNames = null,
@@ -41,8 +48,72 @@ public class TraceSessionAttribute : Attribute
         int[] includedEventIds = null,
         int[] excludedEventIds = null)
     {
-
+        this.providerName = providerName;
+        this.eventParserType = eventParserType;
+        this.sessionName = sessionName;
+        this.providerLevel = providerLevel;
+        this.acceptedEventProviderNames = acceptedEventProviderNames;
+        this.rejectedEventProviderNames = rejectedEventProviderNames;
+        this.acceptedEventNames = acceptedEventNames;
+        this.rejectedEventNames = rejectedEventNames;
+        this.inclusiveProcessNames = inclusiveProcessNames;
+        this.includedEventIds = includedEventIds;
+        this.excludedEventIds = excludedEventIds;
     }
+
+    public TraceEventProviderOptions TraceEventProviderOptions
+    {
+        get
+        {
+            return new TraceEventProviderOptions()
+            {
+                EventIDsToDisable = excludedEventIds,
+                EventIDsToEnable = includedEventIds,
+                ProcessNameFilter = inclusiveProcessNames.ToList()
+            };
+        }
+    }
+
+    public bool IsKernelSession
+    {
+        get
+        {
+            return (sessionName == KernelTraceEventParser.KernelSessionName);
+        }
+    }
+
+    public bool CanProviderNameAccepted(string providerName)
+    {
+        if (rejectedEventProviderNames != null && rejectedEventProviderNames.Contains(providerName)) return false;
+        if (acceptedEventProviderNames != null && acceptedEventProviderNames.Contains(providerName)) return true;
+        return false;
+    }
+
+    public bool CanEventNameAccepted(string eventName)
+    {
+        if (rejectedEventNames != null && rejectedEventNames.Contains(eventName)) return false;
+        if (acceptedEventNames != null && acceptedEventNames.Contains(eventName)) return true;
+        return false;
+    }
+
+    public bool IsEventIdIncluded(int eventId)
+    {
+        if (excludedEventIds != null && excludedEventIds.Contains(eventId)) return false;
+        if (includedEventIds != null && includedEventIds.Contains(eventId)) return true;
+        return false;
+    }
+
+    public readonly string providerName;
+    public readonly Type eventParserType;
+    public readonly string sessionName;
+    public readonly TraceEventLevel providerLevel;
+    public readonly string[] acceptedEventProviderNames;
+    public readonly string[] rejectedEventProviderNames;
+    public readonly string[] acceptedEventNames;
+    public readonly string[] rejectedEventNames;
+    public readonly string[] inclusiveProcessNames;
+    public readonly int[] includedEventIds = null;
+    public readonly int[] excludedEventIds = null;
 }
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
@@ -58,16 +129,109 @@ public class ClrTraceSessionAttribute : TraceSessionAttribute
         string[] inclusiveProcessNames = null,
         int[] includedEventIds = null,
         int[] excludedEventIds = null) : base(ClrTraceEventParser.ProviderName,
+            typeof(ClrTraceEventParser),
             sessionName,
             TraceEventLevel.Verbose,
-        acceptedEventProviderNames,
-        rejectedEventProviderNames,
-        acceptedEventNames,
-        rejectedEventNames,
-        inclusiveProcessNames,
-        includedEventIds,
-        excludedEventIds)
+            acceptedEventProviderNames,
+            rejectedEventProviderNames,
+            acceptedEventNames,
+            rejectedEventNames,
+            inclusiveProcessNames,
+            includedEventIds,
+            excludedEventIds)
     {
 
     }
+}
+
+public class TestTraceSession
+{
+    public readonly TraceSessionAttribute SessionAttribute;
+    public readonly Thread TestTraceThread;
+    public readonly Guid Id;
+
+
+    private TraceEventSession traceEventSession;
+    private TraceEventParser traceEventParser;
+    private readonly int mainTestThreadId;
+
+    public static TestTraceSession Create(TraceSessionAttribute sessionAttribute, int mainTestThreadId)
+    {
+        return new TestTraceSession(sessionAttribute, mainTestThreadId);
+    }
+
+    private TestTraceSession()
+    {
+
+    }
+
+
+    private TestTraceSession(TraceSessionAttribute sessionAttribute, int mainTestThreadId)
+    {
+        SessionAttribute = sessionAttribute;
+        this.mainTestThreadId = mainTestThreadId;
+        Id = Guid.NewGuid();
+
+        traceEventSession = new TraceEventSession(sessionAttribute.sessionName, sessionAttribute.IsKernelSession ? TraceEventSessionOptions.NoRestartOnCreate : TraceEventSessionOptions.Create)
+        {
+            BufferSizeMB = 128,
+            CpuSampleIntervalMSec = 10,
+        };
+
+        TestTraceThread = new Thread(TraceListener)
+        {
+            Priority = ThreadPriority.Lowest,
+            IsBackground = true,
+            Name = "TestTraceThread_" + Id.ToString().ToLower()
+        };
+    }
+
+    private void TraceListener()
+    {
+        try
+        {
+            if (SessionAttribute.IsKernelSession)
+            {
+                if (SessionAttribute is KernelTraceSessionAttribute)
+                {
+                    traceEventSession.EnableKernelProvider(((KernelTraceSessionAttribute)SessionAttribute).KernelSessionKeywords);
+                }
+                else
+                {
+                    traceEventSession.EnableKernelProvider(KernelTraceEventParser.Keywords.Default);
+                }
+                traceEventParser = new KernelTraceEventParser(traceEventSession.Source);
+            }
+            else
+            {
+                traceEventSession.EnableProvider(SessionAttribute.providerName, SessionAttribute.providerLevel, options: SessionAttribute.TraceEventProviderOptions);
+                traceEventParser = Activator.CreateInstance(SessionAttribute.eventParserType) as TraceEventParser;
+            }
+
+
+            traceEventParser.AddCallbackForProviderEvents((providerName, eventName) =>
+            {
+                if (SessionAttribute.CanProviderNameAccepted(providerName) && SessionAttribute.CanEventNameAccepted(eventName))
+                {
+                    return EventFilterResponse.AcceptEvent;
+                }
+                else
+                {
+                    return EventFilterResponse.RejectEvent;
+                }
+            }, (TraceEvent evt) =>
+            {
+                if (evt.ThreadID == mainTestThreadId)
+                {
+                    //accepted;
+                }
+            });
+            traceEventSession.Source.Process();
+        }
+        catch (ThreadAbortException)
+        {
+            traceEventSession.Source.StopProcessing();
+        }
+    }
+
 }
---
+++ UnitTestExtensions/MiniDump.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+/*
+     private static ClrThread GetClrThread(Thread t)
+        {
+
+            ClrRuntime runtime = GetClrRuntime();
+            foreach (ClrThread thread in runtime.Threads)
+            {
+                if (thread.ManagedThreadId == t.ManagedThreadId)
+                {
+                    return thread;
+                }
+            }
+
+            return null;
+        }
+
+
+        private static ClrRuntime GetClrRuntime()
+        {
+            using (DataTarget target = DataTarget.AttachToProcess(Process.GetCurrentProcess().Id, 5000, AttachFlag.Passive))
+            {
+                return target.ClrVersions.First().CreateRuntime();
+            }
+        }
+     
+     */
+
+namespace DumpUtility
+{
+    public static class MiniDump
+    {
+        [DllImport("DbgHelp.dll", CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+        private static extern bool MiniDumpWriteDump(
+                                    IntPtr hProcess,
+                                    Int32 processId,
+                                    IntPtr fileHandle,
+                                    MiniDumpType dumpType,
+                                    ref MinidumpExceptionInfo excepInfo,
+                                    IntPtr userInfo,
+                                    IntPtr extInfo);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        private static extern int GetCurrentThreadId();
+
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetLastError();
+
+        struct MinidumpExceptionInfo
+        {
+            public int ThreadId;
+            public IntPtr ExceptionPointers;
+            public bool ClientPointers;
+        }
+
+        public static bool TryDump(string dmpPath, MiniDumpType dmpType, int ThreadId)
+        {
+            using (FileStream stream = new FileStream(dmpPath, FileMode.Create))
+            {
+                Process process = Process.GetCurrentProcess();
+
+                MinidumpExceptionInfo mei = new MinidumpExceptionInfo
+                {
+                    ThreadId = GetCurrentThreadId(),
+                    ExceptionPointers = Marshal.GetExceptionPointers(),
+                    ClientPointers = true
+                };
+
+                bool res = MiniDumpWriteDump(
+                                    process.Handle,
+                                    process.Id,
+                                    stream.SafeFileHandle.DangerousGetHandle(),
+                                    dmpType,
+                                    ref mei,
+                                    IntPtr.Zero,
+                                    IntPtr.Zero);
+
+                stream.Flush();
+                stream.Close();
+
+                if (!res)
+                {
+                    uint err = GetLastError();
+
+                }
+
+                return res;
+            }
+        }
+    }
+
+    public enum MiniDumpType
+    {
+        None = 0x00010000,
+        Normal = 0x00000000,
+        WithDataSegs = 0x00000001,
+        WithFullMemory = 0x00000002,
+        WithHandleData = 0x00000004,
+        FilterMemory = 0x00000008,
+        ScanMemory = 0x00000010,
+        WithUnloadedModules = 0x00000020,
+        WithIndirectlyReferencedMemory = 0x00000040,
+        FilterModulePaths = 0x00000080,
+        WithProcessThreadData = 0x00000100,
+        WithPrivateReadWriteMemory = 0x00000200,
+        WithoutOptionalData = 0x00000400,
+        WithFullMemoryInfo = 0x00000800,
+        WithThreadInfo = 0x00001000,
+        WithCodeSegs = 0x00002000
+    }
+}
